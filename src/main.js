090df1746342e7c9a00b72b622a2918cf7fb133f
Jason Sutter <jasonsutter@Jasons-MacBook.local>
2019-04-26 10:16:56 -0700

    vue router is working

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import routes from './routes/index'
+import vueRouter from 'vue-router'
+
 
 Vue.config.productionTip = false
 
+Vue.use(vueRouter)
+
+/****************************************/
+/********** Router START here **********/
+/**************************************/
+const router = new vueRouter({
+  routes: routes
+});
+
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
